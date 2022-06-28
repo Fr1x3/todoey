40cda8fe486266c7c7059ef7b22cec60fc166b92
unknown <gathufx@gmail.com>
2022-06-28 07:09:41 -0700

    fix: refactor task tile

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/tasks_list.dart';
+
 
 class TasksScreen extends StatelessWidget {
   const TasksScreen({Key? key}) : super(key: key);
@@ -62,22 +64,6 @@ class TasksScreen extends StatelessWidget {
             ),
           ),
         ],
-      ),
-    );
-  }
-}
-
-
-
-class TaskTile extends StatelessWidget {
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      title: Text('This is a task'),
-      trailing: Checkbox(
-        value: false,
-        onChanged: (prev){},
       ),
     );
   }
